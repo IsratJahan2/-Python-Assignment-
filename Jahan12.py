46953a323facf343530e4cffb139108c39ed7f70
IsratJahan2 <32858411+IsratJahan2@users.noreply.github.com>
2017-10-16 22:42:45 -0400

    Add files via upload

---
+++ Jahan12.py
@@ -0,0 +1,10 @@
+#Jahan12.py
+#A program that use turtle to create a circle
+#Israt Jahan
+import turtle
+def main():
+    for i in range(45):
+        turtle.forward(100)
+        turtle.right(92)
+        
+main()
